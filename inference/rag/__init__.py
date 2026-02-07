01e4eae0ad1f64f93d3277089f688d1c30ef2f99
Ryann <ryanngohenrui@gmail.com>
2026-02-07 21:31:05 +0800

    Update init

---
+++ inference/rag/__init__.py
@@ -0,0 +1,5 @@
+from inference.rag.embeddings import HFEmbeddingModel
+from inference.rag.index import FAISSIndex
+from inference.rag.prompt_builder import RAGPromptBuilder
+
+__all__ = ["FAISSIndex", "HFEmbeddingModel", "RAGPromptBuilder"]
